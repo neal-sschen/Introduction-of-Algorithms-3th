bed16e5001ef839dd9bf2b5e401b2f85f37625e7
neal-sschen <css079@gmail.com>
2018-08-28 17:48:32 +0800

    2.3.2

---
+++ 2_3_2.py
@@ -0,0 +1,43 @@
+def Merge(A,p,q,r):
+    L = A[p:(q+1)]
+    R = A[(q+1):r+1]
+
+#    L.append(10000)
+#    R.append(10000)
+
+    k = p
+    i = 0
+    j = 0
+
+    while(k <= r):
+        if i == len(L):
+            A.extend(R[j:])
+            break
+        elif j == len(R):
+            A.extend(L[i:])
+            break
+        else:
+            if L[i] < R[j]:
+                A[k] = L[i]
+                k = k+1
+                i = i+1
+            else:
+                A[k] = R[j]
+                k = k+1
+                j = j +1
+
+def Merge_Sort(A,p,r):
+    if p < r:
+        q = int((p+r)/2)
+        Merge_Sort(A,p,q)
+        Merge_Sort(A,q+1,r)
+        Merge(A,p,q,r)
+
+
+A = [3,41,20]
+
+
+
+Merge_Sort(A,0,len(A)-1)
+
+print(A)
